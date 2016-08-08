2504d2d91d07a8d3da5ee3eb52b4dbcff8a2659d
Wei Jun <junster90@gmail.com>
2016-08-08 19:25:46 +0800

    added front end styling

--- app/controllers/object_records_controller.rb
+++ app/controllers/object_records_controller.rb
@@ -1,20 +1,16 @@
 class ObjectRecordsController < ApplicationController
-  def new
-  end
-
   def create
     importer = ObjectRecordCsvImporter.new(csv)
 
     importer.import
     flash[:success] = "Successfully imported from CSV!"
-    redirect_to root_path
 
     rescue ArgumentError
       flash[:alert] = "There was a problem with your CSV file."
-      redirect_to new_object_record_path
     rescue => e
       flash[:alert] = e.message
-      redirect_to new_object_record_path
+    ensure
+      redirect_to root_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   root 'homes#index'
-  resources :object_records, only: [:new, :create]
-  resource :object_snapshots, only: [:create]
+  resources :object_records, only: [:create]
+  resources :object_snapshots, only: [:create]
 end
