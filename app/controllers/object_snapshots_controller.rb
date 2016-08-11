4c2f9df09e2642120449dd165376b5c441bfbb1f
Wei Jun <junster90@gmail.com>
2016-08-12 03:22:45 +0800

    prefer passing locals to partial and refactored controller for readability

--- app/controllers/object_snapshots_controller.rb
+++ app/controllers/object_snapshots_controller.rb
@@ -2,14 +2,18 @@ class ObjectSnapshotsController < ApplicationController
   def create
     @result = ObjectSnapshotsBuilder.new(search_query_params).build
 
-    respond_to do |format|
-      @result ? format.js : format.js { render 'error' }
-    end
+    render_result_or_error
   end
 
   private
 
   def search_query_params
     params.require(:search).permit(:object_id, :object_type, :timestamp)
+  end
+
+  def render_result_or_error
+    respond_to do |format|
+      @result ? format.js : format.js { render 'error' }
+    end
   end
 end
